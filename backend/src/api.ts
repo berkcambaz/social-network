f5e4a30b43d4fe6501237c5f065ad2bec104f6c0
Berk Cambaz <berkcambaz12321@gmail.com>
2022-06-07 15:02:11 +0300

    Add db class and more api routes

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,6 +1,8 @@
 import { ApiCode, ApiSchema } from "../../shared/types";
 import { auth } from "./api/auth";
 import { login } from "./api/login";
+import { logout } from "./api/logout";
+import { signup } from "./api/signup";
 import { ReqType, ResType } from "./types";
 
 export class Api {
@@ -12,6 +14,8 @@ export class Api {
     switch (schema.type) {
       case ApiCode.Auth: auth(req, res, schema.data); break;
       case ApiCode.Login: login(req, res, schema.data); break;
+      case ApiCode.Signup: signup(req, res, schema.data); break;
+      case ApiCode.Logout: logout(req, res, schema.data); break;
       default: break;
     }
   }
---
+++ backend/src/api/signup.ts
@@ -0,0 +1,6 @@
+import { ApiCode, ApiReq } from "../../../shared/types";
+import { ReqType, ResType } from "../types";
+
+export function signup(req: ReqType, res: ResType, data: ApiReq[ApiCode.Signup]) {
+
+}
