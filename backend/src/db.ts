50eb61411b022dda37573eb77cf56e724ea6f173
Berk Cambaz <berkcambaz12321@gmail.com>
2022-06-08 12:24:48 +0300

    .env file

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,9 @@
 import * as mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  database: "sheetgram",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -4,3 +4,15 @@ import { IncomingMessage, Server, ServerResponse } from "http"
 
 export type ReqType = FastifyRequest<RouteGenericInterface, Server, IncomingMessage, unknown, FastifyLoggerInstance>
 export type ResType = FastifyReply<Server, IncomingMessage, ServerResponse, RouteGenericInterface, unknown>
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_NAME: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+    }
+  }
+}
